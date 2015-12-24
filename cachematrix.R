fa2d941ced930635d792ece41df2951b94a25598
DanielleReed <reed@monell.org>
2015-12-24 09:10:04 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#two functions check if a precomputed matrix inverse is available to avoid expensive computation
+#code generated using (a) coursework, (b)tips for struggling students and (c) hints from my daugther
+#input 'x' is a matrix to be inverted, e.g., numbers 1, 2, 3, 4 in a two-by-two square
+#the first function creates a list of four functions 
+#the set, get, set inverse and get inverse are steps needed to check if the inverse is already available
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#the second function checks to see if there is a cashed solution
+#if there is a solution (i.e., the get$inverse is not null), it gets it
+#if there is no cache solution, the function 'solve' computes it
+#the last 'i' at the end shows the matrix inverse 
+cacheSolve <- function(x) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
